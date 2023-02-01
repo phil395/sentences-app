56649805633d9a3fb1da7ffb338d0a91845341ab
phil395 <philiplgvn@gmail.com>
2023-02-01 16:24:59 +0300

    add layout for Controls

--- src/components/SentenceBox/Controls.tsx
+++ src/components/SentenceBox/Controls.tsx
@@ -1,4 +1,6 @@
 import type { FC } from "react";
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { HiSpeakerWave } from 'react-icons/hi2';
 
 interface Props {
 
@@ -6,8 +8,12 @@ interface Props {
 
 export const Controls: FC<Props> = () => {
 	return (
-		<div>
-
+		<div className="mt-2 flex justify-end items-center">
+			<button className="btn">Check</button>
+			<button className="btn-outline">Show</button>
+			<button className="btn-outline">Reset</button>
+			<button className="btn-outline rounded-full p-2"><HiSpeakerWave size={15} /></button>
+			<button className="btn-outline rounded-full p-1"><BsThreeDotsVertical size={22} /></button>
 		</div>
 	);
 };
