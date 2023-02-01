12bf4c16d47e47a8266a2f2cd1efbe893e397218
phil395 <philiplgvn@gmail.com>
2023-02-01 14:52:56 +0300

    configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/components/**/*.tsx"],
   theme: {
+    screens: {
+      'xs': '380px',
+      'sm': '480px',
+      'md': '576px',
+      'lg': '768px',
+      'xl': '992px',
+      '2xl': '1110px'
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '10px',
+        'md': '15px'
+      }
+    },
     extend: {
       colors: {
         'light-bg': '#f7f8fb'
