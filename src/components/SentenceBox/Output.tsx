6e49d3e51bcb58dcec8c4e73d10d85f43b8ccd94
phil395 <philiplgvn@gmail.com>
2023-02-01 16:24:39 +0300

    add layout for Output

--- src/components/SentenceBox/Output.tsx
+++ src/components/SentenceBox/Output.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export const Output: FC<Props> = () => {
 	return (
-		<output>
-
+		<output className="block py-4 px-3 rounded-b-xl shadow border border-slate-200 bg-slate-100">
+			Hello my name
 		</output>
 	);
 };
